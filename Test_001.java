7dc341e878eb60068f577bed11448638a08fa833
Sinkorty <70262132+Sinkorty@users.noreply.github.com>
2020-10-05 12:39:17 +0800

    上传了Java测试源文件
    第一次使用GitHub上传文件

---
+++ Test_001.java
@@ -0,0 +1,15 @@
+public class Test_001
+{
+	public static void main(String[] args)
+	{
+		System.out.print("Hello world!");
+	}
+}
+class Test_002
+{
+	public static void main(String[] args)
+	{
+		System.out.print('\u554a');
+		System.out.print('\u8df9');
+	}
+}
