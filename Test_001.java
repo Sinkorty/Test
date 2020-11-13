12278f7f3fbf30e915d9ed0e0789a4509f84ecd9
Sinkorty <70262132+Sinkorty@users.noreply.github.com>
2020-11-13 13:21:10 +0800

    删除了一个类
    233

--- Test_001.java
+++ Test_001.java
@@ -5,11 +5,3 @@ public class Test_001
 		System.out.print("Hello world!");
 	}
 }
-class Test_002
-{
-	public static void main(String[] args)
-	{
-		System.out.print('\u554a');
-		System.out.print('\u8df9');
-	}
-}
